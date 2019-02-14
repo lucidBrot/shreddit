064b9e0266bb9709d201f44194ff9a26315b2a2f
Eric Mink <mink@ubique.ch>
2019-02-14 20:55:15 +0100

    fetching html works

--- app/src/main/java/com/lucidbrot/shreddit/HandleTextIntentActivity.java
+++ app/src/main/java/com/lucidbrot/shreddit/HandleTextIntentActivity.java
@@ -1,36 +1,177 @@
 package com.lucidbrot.shreddit;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.URLUtil;
+import android.webkit.ValueCallback;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class HandleTextIntentActivity extends Activity {
+
+	private View rootView;
 
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_handletextintent);
+		rootView = findViewById(R.id.rootviewlul);
+
+		Log.d("onCreate", "got to here");
 
 		// Get intent, action and MIME type
-		Intent intent = getIntent();
+		final Intent intent = getIntent();
 		String action = intent.getAction();
 		String type = intent.getType();
 
 		if (Intent.ACTION_SEND.equals(action) && type != null) {
 
 			if ("text/plain".equals(type)) {
-				handleSendText(intent); // Handle text being sent
+				Runnable runnable = new Runnable() {
+					@Override
+					public void run() {
+						handleSendText(intent); // Handle text being sent
+					}
+				};
+				Thread t = new Thread(runnable);
+				t.start();
 			}
 		}
 	}
 
 	private void handleSendText(Intent intent) {
 		String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+		Log.d("handleSendText", "got to here with url " + sharedText);
 		if (sharedText != null) {
 			// Update UI to reflect text being shared
-//			Toast.makeText(this, sharedText, Toast.LENGTH_SHORT).show();
+			if (URLUtil.isValidUrl(sharedText)) {
+				if (isImageUrl(sharedText)) {
+					Log.d("handleSendText", "got to here  A");
+					// TODO: good case. direct image share
+					getImage(sharedText);
+				}
+				Log.d("handleSendText", "got to anotherwhere C");
+				// image is somewhere within the page
+				loadImagePageHTML(sharedText);
+			}
+			else {
+				Log.d("handleSendText", "got to there B");
+				notImageUrl(sharedText);
+			}
 		}
+	}
+
+	private void loadImagePageHTML(final String url) {
+		final WebView webView = rootView.findViewById(R.id.webview);
+		webView.post(new Runnable() {
+			@Override
+			public void run() {
+
+				final Context myApp = HandleTextIntentActivity.this;
+
+				/* An instance of this class will be registered as a JavaScript interface */
+				class MyJavaScriptInterface {
+					@JavascriptInterface
+					@SuppressWarnings("unused")
+					public void processHTML(String html) {
+						// process the html as needed by the app
+						Log.d("js tag","le html: "+html);
+					}
+				}
+
+				final WebView browser = webView;
+				/* JavaScript must be enabled if you want it to work, obviously */
+				browser.getSettings().setJavaScriptEnabled(true);
+
+				/* Register a new JavaScript interface called HTMLOUT */
+				browser.addJavascriptInterface(new MyJavaScriptInterface(), "HTMLOUT");
+
+				/* WebViewClient must be set BEFORE calling loadUrl! */
+				browser.setWebViewClient(new WebViewClient() {
+					@Override
+					public void onPageFinished(WebView view, String url) {
+						/* This call inject JavaScript into the page which just finished loading. */
+						browser.loadUrl(
+								"javascript:window.HTMLOUT.processHTML('<head>'+document.getElementsByTagName('html')[0].innerHTML+'</head>');");
+					}
+				});
+
+				/* load a web page */
+				webView.loadUrl(url);
+
+			}
+		});
+	}
+
+	private void getImage(String sharedText) {
+		Log.d("getImage", "got to here");
+		Bitmap image = getBitmapFromURL(sharedText);
+		if (image != null) {
+			showImage(image);
+		}
+	}
+
+	private void showImage(Bitmap image) {
+		((ImageView) findViewById(R.id.imageview)).setImageBitmap(image);
+	}
+
+	public Bitmap getBitmapFromURL(String src) {
+		try {
+			java.net.URL url = new java.net.URL(src);
+			HttpURLConnection connection = (HttpURLConnection) url
+					.openConnection();
+			connection.setDoInput(true);
+			connection.connect();
+			InputStream input = connection.getInputStream();
+			Bitmap myBitmap = BitmapFactory.decodeStream(input);
+			Log.d("getBitmapFromURL", "got image");
+			return myBitmap;
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	private void notImageUrl(final String sharedText) {
+		final Activity activity = this;
+		Log.d("NotImageUrl", "got to here");
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				Toast.makeText(activity, "is not image url: " + sharedText, Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+
+	private boolean isImageUrl(String url) {
+		URLConnection connection = null;
+		try {
+			connection = new URL(url).openConnection();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+			Log.e("isImageUrl", "Failed to fetch " + url);
+		}
+		String contentType = connection.getHeaderField("Content-Type");
+		boolean isImage = contentType.startsWith("image/");
+		return isImage;
 	}
 }
 
